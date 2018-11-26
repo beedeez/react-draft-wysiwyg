74dfb9861eff3c275db62d61a0b1f91b9dfbd1e9
Morgan Laupies <morgan.laupies@beedeez.com>
2018-11-26 12:13:37 +0100

    fixes

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,83 +2,82 @@ const path = require('path');
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-	devtool: 'source-map',
-	entry: ['./src/index'],
-	output: {
-		path: path.join(__dirname, '../dist'),
-		filename: 'react-draft-wysiwyg.js',
-		library: 'reactDraftWysiwyg',
-		libraryTarget: 'umd'
-	},
-	externals: {
-		react: 'react',
-		immutable: 'immutable',
-		'react-dom': 'react-dom',
-		'draft-js': 'draft-js'
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production')
-			}
-		}),
-		new TerserPlugin({
-			terserOptions: {
-				compress: true,
-				output: { comments: false },
-				ecma: 5,
-				ie8: false
-			},
-			parallel: true,
-			sourceMap: true,
-			extractComments: true
-		}),
-		new MiniCssExtractPlugin({
-			// Options similar to the same options in webpackOptions.output
-			// both options are optional
-			filename: '[name].css',
-			chunkFilename: '[id].css'
-		}),
-		new webpack.LoaderOptionsPlugin({
-			options: {
-				postcss: [autoprefixer, precss]
-			}
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /immutable\.js$|draftjs-utils\.js$/
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							// you can specify a publicPath here
-							// by default it use publicPath in webpackOptions.output
-							publicPath: '../'
-						}
-					},
-					'css-loader'
-				]
-			},
-			{ test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
-			{
-				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-				loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.js', '.json']
-	}
+  devtool: 'source-map',
+  entry: ['./src/index'],
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: 'react-draft-wysiwyg.js',
+    library: 'reactDraftWysiwyg',
+    libraryTarget: 'umd',
+  },
+  externals: {
+    react: 'react',
+    immutable: 'immutable',
+    'react-dom': 'react-dom',
+    'draft-js': 'draft-js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new TerserPlugin({
+      terserOptions: {
+        compress: true,
+        output: { comments: false },
+        ecma: 5,
+        ie8: false,
+      },
+      parallel: true,
+      sourceMap: true,
+      extractComments: true,
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [autoprefixer, precss],
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /immutable\.js$|draftjs-utils\.js$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: '../',
+            },
+          },
+          'css-loader',
+        ],
+      },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+  },
 };
